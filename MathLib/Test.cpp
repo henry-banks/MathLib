4a0d50d57bfd2c7e658c3a417a264bbc1c2be190
henry-banks <henry.banks@students.aie.edu.au>
2016-09-26 10:59:47 -0700

    Add project files.

---
+++ MathLib/Test.cpp
@@ -0,0 +1,10 @@
+#include "Test.h"
+#include <iostream>
+
+int doNothing(int x)
+{
+	//This don't work
+	printf("Yo Yo wazzup");
+
+	return x;
+}
---
+++ MathTests/main.cpp
@@ -0,0 +1,12 @@
+#include <cassert>
+#include <iostream>
+
+#include "Test.h"
+
+void main()
+{
+	printf("%d", doNothing(5));
+
+	getchar();
+	return;
+}
